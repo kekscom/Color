33ca77d8be90335cd9c38c3ba92e411c92c2719d
Jan Marsch <jama@keks.com>
2014-01-17 12:22:03 +0100

    new handling of HSL adjustments

--- Color.js
+++ Color.js
@@ -1,15 +1,52 @@
 var Color = (function() {
 
-  var
-    MODE = 'hex',
-    R, G, B, A, HSL;
+  function hue2rgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1/6) return p + (q-p) * 6 * t;
+    if (t < 1/2) return q;
+    if (t < 2/3) return p + (q-p) * (2/3 - t) * 6;
+    return p;
+  }
 
-  function toHSL() {
+  function limit(v, max) {
+    return Math.min(max, Math.max(0, v));
+  }
+
+  /*
+   * str can be in any of these:
+   * #0099ff #0099ffAA #09f #09fA
+   * rgb(64, 128, 255) rgba(64, 128, 255, 0.5)
+   */
+  function Color(str) {
     var
-      r = R/255,
-      g = G/255,
-      b = B/255,
-      max = Math.max(r, g, b), min = Math.min(r, g, b),
+      r = g = b = 0,
+      a = 1,
+      m;
+
+    str += '';
+    if (m = str.match(/^#(\w{2})(\w{2})(\w{2})(\w{2})?$/)) {
+      this.hex = true;
+      r = parseInt(m[1], 16);
+      g = parseInt(m[2], 16);
+      b = parseInt(m[3], 16);
+      a = m[4] ? parseInt(m[4], 16) / 255 : 1;
+    }
+
+    if (m = str.match(/rgba?\((\d+)\D+(\d+)\D+(\d+)(\D+([\d.]+))?\)/)) {
+      r = parseInt(m[1], 10);
+      g = parseInt(m[2], 10);
+      b = parseInt(m[3], 10);
+      a = m[4] ? parseFloat(m[5]) : 1;
+    }
+
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    var
+      max = Math.max(r, g, b),
+      min = Math.min(r, g, b),
       h, s, l = (max+min) / 2,
       d = max-min;
 
@@ -22,96 +59,45 @@ var Color = (function() {
         case g: h = (b-r) / d + 2; break;
         case b: h = (r-g) / d + 4; break;
       }
-      h /= 6;
+      h *= 60;
     }
 
-    HSL = { h:h*360, s:s, l:l };
-  };
-
-  function toRGB() {
-    if (HSL.s === 0) {
-      R = G = B = HSL.l; // achromatic
-      return;
-    }
-
-    var
-      q = HSL.l < 0.5 ? HSL.l * (1+HSL.s) : HSL.l + HSL.s - HSL.l*HSL.s,
-      p = 2 * HSL.l-q;
-      HSL.h /= 360;
-
-    R = hue2rgb(p, q, HSL.h + 1/3) * 255 <<0;
-    G = hue2rgb(p, q, HSL.h      ) * 255 <<0;
-    B = hue2rgb(p, q, HSL.h - 1/3) * 255 <<0;
-  }
-
-  function hue2rgb(p, q, t) {
-    if (t < 0) t += 1;
-    if (t > 1) t -= 1;
-    if (t < 1/6) return p + (q-p) * 6 * t;
-    if (t < 1/2) return q;
-    if (t < 2/3) return p + (q-p) * (2/3 - t) * 6;
-    return p;
-  }
-
-  function limit(val, max) {
-    return Math.min(max, Math.max(0, val));
-  }
-
-  /*
-   * str can be in any of these:
-   * #0099ff #0099ffAA #09f #09fA
-   * rgb(64, 128, 255) rgba(64, 128, 255, 0.5)
-   */
-  function Color(str) {
-    var m;
-    str += '';
-    if (m = str.match(/^#(\w{2})(\w{2})(\w{2})(\w{2})?$/)) {
-      R = parseInt(m[1], 16);
-      G = parseInt(m[2], 16);
-      B = parseInt(m[3], 16);
-      A = m[4] ? parseInt(m[4], 16) / 255 : 1;
-    }
-
-    if (m = str.match(/rgba?\((\d+)\D+(\d+)\D+(\d+)(\D+([\d.]+))?\)/)) {
-      MODE = 'rgb'
-      R = parseInt(m[1], 10);
-      G = parseInt(m[2], 10);
-      B = parseInt(m[3], 10);
-      A = m[4] ? parseFloat(m[5]) : 1;
-    }
+    this.H = h;
+    this.S = s;
+    this.L = l;
+    this.A = a;
   };
 
   var proto = Color.prototype;
 
-  proto.hue = function(h) {
-    if (!HSL) toHSL();
-    HSL.h = limit(HSL.h*h, 360);
-    return this;
-  };
-
-  proto.saturation = function(s) {
-    if (!HSL) toHSL();
-    HSL.s = limit(HSL.s*s, 1);
-    return this;
-  };
-
-  proto.lightness = function(l) {
-    if (!HSL) toHSL();
-    HSL.l = limit(HSL.l*l, 1);
-    return this;
-  };
-
-  proto.alpha = function(a) {
-    A = limit(A*a, 1);
-    return this;
-  };
-
   proto.toString = function() {
-    if (HSL) toRGB();
-    if (MODE === 'hex') {
-      return '#' + ((1 <<24) + (R <<16) + (G <<8) + B).toString(16).slice(1, 7) + (A < 1 ? (A*256<<0).toString(16) : '');
+    var 
+      h = limit(this.H, 360),
+      s = limit(this.S, 1),
+      l = limit(this.L, 1),
+      a = limit(this.A, 1),
+      r, g, b;
+
+    // achromatic
+    if (s === 0) {
+      r = l;
+      g = l;
+      b = l;
+    } else {
+      var
+        q = l < 0.5 ? l * (1+s) : l + s - l*s,
+        p = 2 * l-q;
+        h /= 360;
+
+      r = hue2rgb(p, q, h + 1/3) * 255;
+      g = hue2rgb(p, q, h      ) * 255;
+      b = hue2rgb(p, q, h - 1/3) * 255;
     }
-    return 'rgba(' + [R <<0, G <<0, B <<0, A.toFixed(2)].join(',') + ')';
+
+    if (this.hex) {
+      return '#' + ((1 <<24) + (r <<16) + (g <<8) + b).toString(16).slice(1, 7) + (a < 1 ? (a*256<<0).toString(16) : '');
+    }
+    return 'rgba(' + [r <<0, g <<0, b <<0, a.toFixed(2)].join(',') + ')';
   };
   
   return Color;
